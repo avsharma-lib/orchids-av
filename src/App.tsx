24ba2a676034ee9388d45168b89725c87678532b
avsharma-lib <aaryaveersharma16@gmail.com>
2026-04-16 19:12:05 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState, useRef } from 'react';
 import { motion, useScroll, useTransform } from 'framer-motion';
-import { CheckCircle2, ArrowRight, Instagram, VolumeX, Volume2, Plus, X, ExternalLink, MessageCircle, UserPlus, Phone } from 'lucide-react';
 
 interface Product {
   id: number;
